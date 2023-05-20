223211a4b8e809462d79a2ea779db2d6178e03f5
Pyochangwoo <127657092+Pyochangwoo@users.noreply.github.com>
2023-05-20 21:25:17 +0900

    데이터베이스 생성 및 자동으로 데이터베이스 생성해주는 exe 파일 생성
    exe 파일 실행시 db 와 테이블 그리고 쿼리가 자동으로 생성됨

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, jsonify
+import subprocess
 
 app = Flask(__name__)
+execution_complete = False
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route('/')
+def index():
+    return render_template('loading.html')
+
+@app.route('/main')
+def main():
+    return render_template('index.html')
+
+@app.route('/contests')
+def contests():
+    return render_template('contests.html')
+
+@app.route('/issue')
+def isue():
+    return render_template('issue.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    input_text = request.form.get('input_text')
+    output_text = input_text.upper()
+    return render_template('result.html', input_text=input_text, output_text=output_text)
+
+
+#파이썬 스크립트 파일 실행 #
+@app.route('/execute_python_script')
+def execute_python_script():
+    # 파이썬 파일 실행
+    subprocess.call(['python', 'static/py/script.py'])
+    
+        # 응답 생성
+    response = {
+        'message': '파이썬 파일 실행이 완료되었습니다.',
+    }
+    
+    # 실행이 끝나면 JSON 응답 반환
+    return jsonify(response)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=11559)
