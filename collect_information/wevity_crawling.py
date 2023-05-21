27e2b98c8d79d249403e116f9b28ef9b0268eb65
ShowCreate <qkrwjddls112@naver.com>
2023-05-21 14:52:53 +0900

    비티 사이트를 이용하여 크롤러 뼈대 제작

--- collect_information/wevity_crawling.py
+++ collect_information/wevity_crawling.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # 크롬 드라이버 자동 업데이트을 위한 모듈
 from webdriver_manager.chrome import ChromeDriverManager
@@ -19,8 +20,23 @@ service = Service(executable_path=ChromeDriverManager().install())
 # Chrome 옵션 설정
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# 암묵적으로 웹 자원 로드를 위해 3초 기다림
-driver.implicitly_wait(3)
+# 제목, 주최/주관, 접수기간, 홈페이지를 담을 변수 선언 
+info_titles = []
+info_hosts = []
+info_remainDates = []
+info_urls = []
 
-# 웹페이지 해당 주소 이동
-driver.get("https://www.naver.com")
+# 대회 정보 크롤링 함수
+def crawl_competition_info():
+    field = [20, 21] # 크롤링할 분야 설정 (웹/모바일/IT/게임/소프트웨어)
+
+    for i in field: 
+        page = 1 # 크롤링할 page 갯수 설정 
+        
+        while (page > 0):    
+            driver.get("https://www.wevity.com/?c=find&s=1&gub=1&cidx={}&gp={}".format(i, page))  # 크롤링하려는 웹 페이지 URL 입력
+
+            page = page - 1
+    
+# 대회 정보 크롤링 실행
+crawl_competition_info()
