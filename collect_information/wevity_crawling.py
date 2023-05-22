350b9abf4a215f7b5234d48cb6017a1b52343b0b
ShowCreate <qkrwjddls112@naver.com>
2023-05-22 23:36:44 +0900

    Update wevity_crawling.py 사이트 내 여러 url에 접속하는 기능 구현

--- collect_information/wevity_crawling.py
+++ collect_information/wevity_crawling.py
@@ -20,23 +20,48 @@ service = Service(executable_path=ChromeDriverManager().install())
 # Chrome 옵션 설정
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# 제목, 주최/주관, 접수기간, 홈페이지를 담을 변수 선언 
+# 제목 / 도메인 / 정보 / 접수일 / 마감일 / 이미지 주소
 info_titles = []
-info_hosts = []
-info_remainDates = []
-info_urls = []
+info_domains = []
+info_descriptions = []
+info_startDays = []
+info_endDays = []
+info_imgUrls = []
 
-# 대회 정보 크롤링 함수
-def crawl_competition_info():
-    field = [20, 21] # 크롤링할 분야 설정 (웹/모바일/IT/게임/소프트웨어)
+field = [20, 21] # 크롤링할 분야 설정 (웹/모바일/IT/게임/소프트웨어)
 
-    for i in field: 
-        page = 1 # 크롤링할 page 갯수 설정 
+for i in field: 
+    page = 1 # 크롤링할 page 갯수 설정 
+    while (page > 0):    
+        driver.get("https://www.wevity.com/?c=find&s=1&gub=1&cidx={}&gp={}".format(i, page))  # 크롤링하려는 웹 페이지 URL 입력
+        driver.implicitly_wait(5) # 웹 페이지 로딩 5초 기다림
+
+        for j in range(2, 10):
+            driver.find_element(by=By.XPATH, value='//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li[%d]/div[1]/a' %j).click()
+            driver.implicitly_wait(5)
+            driver.back() # 뒤로 가기
+        # domain = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/div[2]/div[1]/div[2]/div[3]/div/ul/li[2]/div[1]/a')
+
+        # description = driver.find_element(By.CSS_SELECTOR, 'tit > a')
+        # startDay = driver.find_element(By.CSS_SELECTOR, 'tit > a')
+        # endDay = driver.find_element(By.CSS_SELECTOR, 'tit > a')
+        # imgUrl = driver.find_element(By.CSS_SELECTOR, 'tit > a')
         
-        while (page > 0):    
-            driver.get("https://www.wevity.com/?c=find&s=1&gub=1&cidx={}&gp={}".format(i, page))  # 크롤링하려는 웹 페이지 URL 입력
+        # 접수 마감을 했다면 break
+        # if driver.find_element(By.TAG_NAME, '/html/body/div[2]/div[4]/div[2]/div[1]/div[2]/div[3]/div/ul/li[29]/div[3]/span'):
+        #     break
 
-            page = page - 1
-    
-# 대회 정보 크롤링 실행
-crawl_competition_info()
+    page = page - 1
+
+# 크롤러 종료
+driver.quit()
+
+"""
+# selenium 사용법
+driver.find_element(By.CSS_SELECTOR, "CSS선택자")
+driver.find_element(By.XPATH, "XPATH")
+driver.find_element(By.NAME, "NAME속성값")
+driver.find_element(By.CLASS_NAME, "CLASS속성값")
+driver.find_element(By.LINK_TEXT, "LINK텍스트")
+driver.find_element(By.ID, "ID속성값")
+"""
